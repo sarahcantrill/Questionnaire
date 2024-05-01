ec2663ffd827a12860b4214e55f241730d5067e0
sarahcantrill <25324420@edgehill.ac.uk>
2024-05-01 08:20:45 +0000

    select questionnaire styling

--- Questionnaire/resources/views/selectquestionnaire.blade.php
+++ Questionnaire/resources/views/selectquestionnaire.blade.php
@@ -4,36 +4,77 @@
       <meta charset="UTF-8">
       <title>Select a Questionnaire !</title>
       <!-- <link rel="stylesheet" href="/css/app.css" /> -->
+      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
       <style>
-        <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
-                margin: 0;
             }
+
+            .p-inline {
+                display: inline-block;
+                margin-right: 10px;
+                padding: 20px;
+            }
+
+            .home-btn {
+                margin-left: 0px;
+                color: black;
+                text-transform: uppercase;
+                border: 1px black;
+            }
+
+            .row {
+                
+                text-align: center;
+            }
+
+            .title{
+                text-align: center;
+            }
+
+            .p-inline {
+            display: inline-block;
+            color: black;
+            margin-right: 10px;
+            padding: 20px; /* Add padding around the ID */
+            border: 1px solid #ccc; /* Add border around the ID */
+            border-radius: 4px; /* Optional: Add rounded corners */
+            background-color: lightgray;
+        }
+
+
       </style>
   </head>
   <body>
   <div class="container">
-      <header class="row">
+      <header class="home-btn">
       <a href="{{ route('welcome') }}" class="btn btn-secondary">Home</a>
       </header>
+            <div class="title">
+            <h1>Select a Questionnaire:</h1>
+            </div>
+
+
       <article class="row">
-          <h1>Select a Questionnaire:</h1>
 
             @foreach($questionnaires as $questionnaire)
+                <section class="box">
                 <a href="{{ route('questions', ['id' => $questionnaire->id]) }}">
-                <p> ID:{{ $questionnaire->id }}</p>
-                <p> {{ $questionnaire->title }}</p>
+                <p class="p-inline">{{ $questionnaire->id }} </p> 
+                <p class="p-inline"> {{ $questionnaire->title }}</p>
+                <br>
+                </a>
+                </section>
+                
                 
             @endforeach
 
       </article>
-    <!--  
-  </div><close container -->
+   
 
   </body>
   </html>
